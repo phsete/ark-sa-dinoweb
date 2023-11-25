2a365d83cac2c299b6cecb04232a3f86001d41dd
phsete <philteb@gmx.de>
2023-11-25 13:22:53 +0100

    add export to json

--- get_all.py
+++ get_all.py
@@ -1,4 +1,6 @@
 import parse
+import pydantic
+import os
 
 REX_SAVE_NAME = "/Game/PrimalEarth/Dinos/Rex/Rex_Character_BP.Rex_Character_BP_C"
 REX_STATS_NAME = "/Game/PrimalEarth/CoreBlueprints/DinoCharacterStatusComponent_BP_Rex.DinoCharacterStatusComponent_BP_Rex_C"
@@ -7,5 +9,17 @@ dinos = parse.get_matching_dinos(name_filter=REX_SAVE_NAME)
 dinos = {
     REX_SAVE_NAME: [dino2 for dino2 in dinos[REX_SAVE_NAME] if dino2.stats_raw != b'']
 }
-for dino in dinos[REX_SAVE_NAME]:
-    print(dino)
+os.makedirs('output', exist_ok=True)
+with open("output/dinos.json", "w+") as f:
+    f.write("{\n\t\"" + REX_SAVE_NAME + "\": [\n\t\t")
+    i = 0
+    for dino in dinos[REX_SAVE_NAME]:
+        i += 1
+        f.write(dino.model_dump_json(exclude=("info_raw", "stats_raw")))
+        if i < len(dinos[REX_SAVE_NAME]):
+            f.write(",\n\t\t")
+        else:
+            f.write("\n\t")
+    f.write("]\n}")
+# for dino in dinos[REX_SAVE_NAME]:
+#     print(dino)
